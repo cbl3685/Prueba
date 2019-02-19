e443185ae5877d73b9edb42defeda1b04aa900da
Cristobal <cbl3685@gmail.com>
2019-02-18 18:49:10 -0600

    Agregando números random

--- Principal.java
+++ Principal.java
@@ -1,5 +1,8 @@
+import java.lang.Math;
+
 class Principal{
   public static void main(String[] args) {
-    
+    int random = (int)(Math.random() * 10 + 1);
+    System.out.println(random);
   }
 }

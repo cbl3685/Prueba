2d9f5eb76feb90de5f314fde84ac294835c688b7
Cristobal <cbl3685@gmail.com>
2019-02-18 19:09:25 -0600

    Agregando par-impar

--- Principal.java
+++ Principal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 class Principal{
   public static void main(String[] args) {
-    
+    Scanner teclado = new Scanner(System.in);
+    int numero = 0;
+
+    System.out.print("Ingrese un numero entero: ");
+    numero = (int)teclado.nextDouble();
+    if(numero % 2 == 0){
+      System.out.println("El numero "+numero+" es par");
+    }else{
+      System.out.println("El numero "+numero+" es impar");
+    }
   }
 }

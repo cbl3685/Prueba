cf7de7d33a8df5540dbbbbd9c6adbcfc188cf6cf
Cristobal <cbl3685@gmail.com>
2019-02-18 19:03:14 -0600

    Agregando fecha actual

--- Principal.java
+++ Principal.java
@@ -1,5 +1,8 @@
+import java.time.LocalDate;
+
 class Principal{
   public static void main(String[] args) {
-    
+    LocalDate fecha = LocalDate.now();
+    System.out.println("Fecha actual: "+fecha);
   }
 }

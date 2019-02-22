25bbd38905ff3fdacd54076188375f265739f87b
Cristobal <cbl3685@gmail.com>
2019-02-21 19:52:25 -0600

    Agregando el método para mostrar la fecha

--- Principal.java
+++ Principal.java
@@ -2,6 +2,10 @@ import java.time.LocalDate;
 
 class Principal{
   public static void main(String[] args) {
+    mostrarFecha();
+  }
+
+  public static void mostrarFecha(){
     LocalDate fecha = LocalDate.now();
     System.out.println("Fecha actual: "+fecha);
   }
